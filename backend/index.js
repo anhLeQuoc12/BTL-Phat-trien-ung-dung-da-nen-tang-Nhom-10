2b5240b1695db78d5318482c824b4c00877ee899
MARIS_LAPTOP\letua <letuandat556@gmail.com>
2024-11-24 18:00:46 +0700

    update

--- backend/index.js
+++ backend/index.js
@@ -2,12 +2,14 @@ const express = require("express");
 const dotenv = require("dotenv");
 const recipeRoutes = require("./routes/recipe");
 const mealPlanRoutes = require("./routes/mealPlan");
+const mealSuggestRoutes = require("./routes/mealSuggest");
 
 const app = express();
 dotenv.config();
 
 app.use("/api/recipe", recipeRoutes);
 app.use("/api/mealPlan", mealPlanRoutes);
+app.use("/api/mealSuggest", mealSuggestRoutes);
 
 const PORT = process.env.PORT || 8000;
 app.listen(PORT, () => {
--- backend/routes/mealSuggest.js
+++ backend/routes/mealSuggest.js
@@ -5,4 +5,4 @@ const router = express.Router();
 
 router.get("/meal-suggestion", getSuggestMeal);
 
-exports.router = router;
+module.exports = router;
