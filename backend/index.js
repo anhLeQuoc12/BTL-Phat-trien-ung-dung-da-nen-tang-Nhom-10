7c7ec5c0066c0073d8f81aaeec61100c92d9e927
MARIS_LAPTOP\letua <letuandat556@gmail.com>
2024-11-24 17:20:04 +0700

    add mealPlan api

--- backend/index.js
+++ backend/index.js
@@ -1,11 +1,13 @@
 const express = require("express");
 const dotenv = require("dotenv");
 const recipeRoutes = require("./routes/recipe");
+const mealPlanRoutes = require("./routes/mealPlan");
 
 const app = express();
 dotenv.config();
 
 app.use("/api/recipe", recipeRoutes);
+app.use("/api/mealPlan", mealPlanRoutes);
 
 const PORT = process.env.PORT || 8000;
 app.listen(PORT, () => {
