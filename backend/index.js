4b2b491b5096fc82ffc9d8e680d4080e564de3de
nguyendinhutbiu <nguyendinhutbiu@gmail.com>
2024-12-08 21:54:06 +0700

    fix: code

--- backend/index.js
+++ backend/index.js
@@ -1,6 +1,11 @@
 const express = require("express");
 const dotenv = require("dotenv");
 const bodyParser = require("body-parser");
+const auth = require("./routes/auth");
+const recipeRoutes = require("./routes/recipe");
+const mealPlanRoutes = require("./routes/mealPlan");
+const mealSuggestRoutes = require("./routes/mealSuggest");
+const weeklyShoppingReportRoutes = require("./routes/weekly-shopping-report");
 const adminRoute = require("./routes/admin.route");
 const { default: mongoose } = require("mongoose");
 
@@ -11,6 +16,11 @@ app.use(bodyParser.urlencoded({ extended: true }));
 
 mongoose.connect(process.env.MONGODB_URL);
 
+app.use("/api/recipe", recipeRoutes);
+app.use("/api/report-by-weeks", weeklyShoppingReportRoutes)
+app.use("/api/mealPlan", mealPlanRoutes);
+app.use("/api/mealSuggest", mealSuggestRoutes);
+app.use("/api/auth", auth);
 app.use("/api/admin", adminRoute);
 
 const PORT = process.env.PORT || 8000;
