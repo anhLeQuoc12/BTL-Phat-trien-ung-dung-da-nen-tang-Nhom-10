0a6efc92e31d7cafa6719cac9c9cd086c27b0106
MARIS_LAPTOP\letua <letuandat556@gmail.com>
2024-12-08 15:52:22 +0700

    update auth

--- backend/routes/auth.js
+++ backend/routes/auth.js
@@ -5,13 +5,16 @@ const User = require('../models/user.model');
 const jwt = require('jsonwebtoken');
 
 router.post('/login', async (req, res) => {
-    const { username, password } = req.body;
+    const { phone , password } = req.body;
     try {
-        const user = await User.findOne({ username });
-        if (!user || !(await user.comparePassword(password))) {
-            return res.status(401).json({ message: 'Invalid username or password' });
+        const user = await User.findOne({ phone });
+        if (!user ) {
+            return res.status(401).json({ message: 'Invalid users phone ' });
         }
-        const token = jwt.sign({ userId: user._id }, 'your_jwt_secret', { expiresIn: '1h' });
+        if (user.password !== password) {
+            return res.status(401).json({ message: 'Invalid password' });
+        }
+        const token = jwt.sign({ userId: user._id }, process.env.JWT_SECRETKEY, { expiresIn: '1h' });
         res.status(200).json({ token });
     } catch (error) {
         res.status(500).json({ error: error.message });
