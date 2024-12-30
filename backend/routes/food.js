908f089b89d73984709d974666fb692378f5bebe
Puczaee <phucnen069.com>
2024-12-30 11:01:09 +0700

    update food

---
+++ backend/routes/food.js
@@ -0,0 +1,14 @@
+const express = require('express');
+const authMiddleware = require('../middlewares/auth');
+const { addFood, updateFood, deleteFood, getFoodsByCategory, getAllCategories } = require('../controllers/food');
+const {getAllUnits} = require("../services/unitManagement.service");
+
+const router = express.Router();
+router.post('/', authMiddleware, addFood);
+router.put('/', authMiddleware, updateFood);
+router.delete('/', authMiddleware, deleteFood);
+router.get('/category/:id', authMiddleware, getFoodsByCategory);
+router.get('/category', authMiddleware, getAllCategories);
+
+module.exports = router;
+
